22ca1c3a221a592ca056d0be37cc3b2874ab2c08
Evan Snyder <evansnyder16@gmail.com>
2024-09-02 09:46:53 -0400

    Added source files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include "raylib.h"
+
+constexpr int screenWidth = 800;
+constexpr int screenHeight = 450;
+
+int main()
+{
+	InitWindow(screenWidth, screenHeight, "Hello, game programming!");
+	SetTargetFPS(60);
+
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Hello, game development!", 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
